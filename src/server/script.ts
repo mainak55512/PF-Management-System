d1175f0a4fa508723c8abeb66ebcf3b46c1e8cd5
System Administrator <admin@example.com>
2025-04-24 01:34:04 +0530

    Next Commit

--- src/server/script.ts
+++ src/server/script.ts
@@ -1,9 +1,11 @@
 import { gs } from '@servicenow/glide'
 
 export function showStateUpdate(current, previous): void { 
-    const currentState: string = current.getValue('state')
-    const previousState: string = previous.getValue('state')
+    // const currentState: string = current.getValue('state')
+    // const previousState: string = previous.getValue('state')
 
     // gs.addInfoMessage(`state updated from "${previousState}" to "${currentState}"`)
-    gs.addInfoMessage("Checking Typescript!");
+    // gs.addInfoMessage("Checking Typescript!");
+    current.u_uan = "UAN"+current.getValue('u_uan');
+    current.update();
 }
